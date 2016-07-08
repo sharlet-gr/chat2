79cc6c8f0c3c1bb9f344a6346b1cfa6f23d26d2c
sharlet-gr <swilson@grepruby.com>
2016-07-08 19:35:50 +0530

    end session from all tabs

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
   sender=document.getElementById('sender').innerHTML;
   receiver=document.getElementById('receiver').innerHTML;
   socket.emit('userjoin', sender, receiver);
-  
+  $("#logout").click(function(){
+    socket.emit('logout');
+    window.location.assign('/logout');
+  });
 });
 
 function submitfunction(){
@@ -18,11 +21,18 @@ function submitfunction(){
   return false;
 }
 
+
 socket.on('chatMessage', function(from, message, to){
   if(sender==from)
     $('#messages').append('<li><b style="color:green">Me: </b>' + message + '</li>');
   else
     $('#messages').append('<li><b style="color:#009afd">' + from + '</b>: ' + message + '</li>');
+});
+
+socket.on('sessionEnd',function()
+{
+  alert("This session has expired");
+  window.location.assign('/index');
 });
  
 
